7efafca16bd83dadcfe200b8bcd526a58978ed5d
Hirokazu Chiba <hchiba1@gmail.com>
2023-11-21 16:50:30 +0900

    Update site/

---
+++ site/render-table.js
@@ -0,0 +1,103 @@
+$(function () { // When DOM is ready
+  $.get('../tsv/organisms', (res) => {
+    setupAutocomplete(res.trim().split('\n'));
+    setupAutocomplete2(res.trim().split('\n'));
+  });
+  $('#tags').focus();
+
+  const tags = document.getElementById("tags");
+  const tags2 = document.getElementById("tags2");
+  const inputText = document.getElementById("inputText");
+  const submitButton = document.getElementById("submitButton");
+
+  submitButton.addEventListener("click", function () {
+    submitText(inputText.value);
+  });
+
+  inputText.addEventListener("keydown", function (event) {
+    if (event.key === "Enter" && event.ctrlKey) {
+      event.preventDefault(); // デフォルトのEnterキーの動作をキャンセル
+      submitText(inputText.value);
+    }
+  });
+
+  function submitText(text) {
+    const lines = text.split(/\r\n|\r|\n/)
+        .map(line => line.trim().replace(/^\s+/, ""))
+        .filter(line => line !== "") // remove empty lines
+        .map(line => `(ncbigene:${line})`).join(" ");
+    fetchDatabySPARQL(tags.value, tags2.value, lines).then(data => {
+      renderTable(data);
+    });
+  }
+
+  function setupAutocomplete(candidates) {
+    $('#tags').autocomplete({
+      source: (request, response) => {
+        response(
+          $.grep(candidates, (value) => {
+            let regexp = new RegExp('\\b' + escapeRegExp(request.term), 'i');
+            return value.match(regexp);
+          })
+        );
+      },
+      autoFocus: true,
+      delay: 100,
+      minLength: 2,
+    });
+  }
+
+  function setupAutocomplete2(candidates) {
+    $('#tags2').autocomplete({
+      source: (request, response) => {
+        response(
+          $.grep(candidates, (value) => {
+            let regexp = new RegExp('\\b' + escapeRegExp(request.term), 'i');
+            return value.match(regexp);
+          })
+        );
+      },
+      autoFocus: true,
+      delay: 100,
+      minLength: 2,
+    });
+  }
+});
+
+function escapeRegExp(string) {
+  return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
+}
+function renderTable(data) {
+  const table = document.getElementById('resultsTable');
+  table.innerHTML = '';
+
+  const thead = document.createElement('thead');
+  const headerRow = document.createElement('tr');
+  data.head.vars.forEach(variable => {
+    const th = document.createElement('th');
+    th.textContent = variable;
+    headerRow.appendChild(th);
+  });
+  thead.appendChild(headerRow);
+  table.appendChild(thead);
+
+  const tbody = document.createElement('tbody');
+  data.results.bindings.forEach(binding => {
+    const tr = document.createElement('tr');
+    data.head.vars.forEach(variable => {
+      const td = document.createElement('td');
+      const value = binding[variable].value;
+      if (value.match(/^http/)) {
+        let link = document.createElement('a');
+        link.href = value;
+        link.textContent = value.replace(/.*\//, '');
+        td.appendChild(link);
+      } else {
+        td.textContent = value;
+      }
+      tr.appendChild(td);
+    });
+    tbody.appendChild(tr);
+  });
+  table.appendChild(tbody);
+}
---
+++ site/sparql.js
@@ -0,0 +1,44 @@
+async function fetchDatabySPARQL(taxname1, taxname2, input) {
+  const endpointUrl = 'https://spang.dbcls.jp/sparql';
+  const sparqlQuery = `
+PREFIX orth: <http://purl.org/net/orth#>
+PREFIX taxid: <http://identifiers.org/taxonomy/>
+PREFIX ncbigene: <http://identifiers.org/ncbigene/>
+PREFIX dct: <http://purl.org/dc/terms/>
+PREFIX : <https://dbcls.github.io/ncbigene-rdf/ontology.ttl#>
+
+SELECT DISTINCT ?gene1 ?label1 ?description1 ?gene2 ?label2 ?description2
+WHERE {
+  VALUES (?gene1) { ${input} }
+  ?gene1 orth:hasOrtholog+ ?gene2 .
+  ?gene1 :taxid ?taxid1 .
+  ?gene1 rdfs:label ?label1 .
+  ?gene1 dct:description ?description1 .
+  ?taxid1 rdfs:label "${taxname1}" .
+  ?gene2 :taxid ?taxid2 .
+  ?gene2 rdfs:label ?label2 .
+  ?gene2 dct:description ?description2 .
+  ?taxid2 rdfs:label "${taxname2}" .
+}
+`;
+
+  try {
+    const response = await fetch(endpointUrl, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/sparql-results+json',
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: `query=${encodeURIComponent(sparqlQuery)}`
+    });
+    if (!response.ok) {
+      // response.ok === true if the status code is 2xx
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error(`Could not fetch data: ${error}`);
+  }
+}
+
